78500b3bb2832a03e7a4f08cffb2ab231e0bf77a
Erishen <leisun8309@gmail.com>
2024-11-09 14:29:49 +0800

    add test:coverage

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,14 @@ module.exports = {
   },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
-  }
+  },
+  collectCoverage: true, // 启用覆盖率收集
+  collectCoverageFrom: [
+    'client/**/*.{js,jsx,ts,tsx}', // 指定要包含的文件范围
+    'pages/**/*.{js,jsx,ts,tsx}',
+    'server/**/*.{js,jsx,ts,tsx}',
+    'src/**/*.{js,jsx,ts,tsx}'
+  ],
+  coverageDirectory: 'coverage', // 覆盖率报告生成的目录
+  coverageReporters: ['json', 'lcov', 'text', 'clover'], // 报告格式
 };
